2cfd3295321730bfdcb602d06f6b29c007f466aa
Rogee <rogee@ipao.vip>
2026-03-04 09:52:19 +0800

    feat: add LRU cache module

---
+++ server/cache.js
@@ -0,0 +1,36 @@
+export class LRUCache {
+  constructor(maxSize = 100) {
+    this.maxSize = maxSize
+    this.cache = new Map()
+  }
+
+  get(key) {
+    if (!this.cache.has(key)) return undefined
+    const value = this.cache.get(key)
+    this.cache.delete(key)
+    this.cache.set(key, value)
+    return value
+  }
+
+  set(key, value) {
+    if (this.cache.has(key)) {
+      this.cache.delete(key)
+    } else if (this.cache.size >= this.maxSize) {
+      const firstKey = this.cache.keys().next().value
+      this.cache.delete(firstKey)
+    }
+    this.cache.set(key, value)
+  }
+
+  has(key) {
+    return this.cache.has(key)
+  }
+
+  get size() {
+    return this.cache.size
+  }
+
+  clear() {
+    this.cache.clear()
+  }
+}
